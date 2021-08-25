4ed9117f7ae0d0922c154868ef5e258c7d4e1ab1
thaynar96 <thaynarbo@gmail.com>
2021-08-25 10:58:09 -0300

    adicionando altura dinamicamente

--- scroll/app.js
+++ scroll/app.js
@@ -7,7 +7,20 @@
 const date = document.getElementById("date");
 date.innerText = new Date().getFullYear;
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
+navToggle.addEventListener("click", function () {
+	const containerHeight = linksContainer.getBoundingClientRect().height;
+	const linksHeight = links.getBoundingClientRect().height;
+
+	if (containerHeight === 0) {
+		linksContainer.style.height = `${linksHeight}px`;
+	} else {
+		linksContainer.style.height = 0;
+	}
+});
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
